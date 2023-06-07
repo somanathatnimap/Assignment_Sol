9b63a85abf306429197859907ec19adb92f0f529
Somanath Barge <somanath@nimap.com>
2023-06-07 15:06:54 +0530

    6th code

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { /*
+           stack is linear datastructure it works on FILO 
+           */
+            Console.WriteLine("Subject Names are :");
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Science");
+            stack.Push("Maths");
+            stack.Push("Marathi");
+            stack.Push("History");
+            stack.Push("English");
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("Total Number of Subjects are "+ stack.Count);
+            Console.ReadLine();
+        }
+    }
+}
