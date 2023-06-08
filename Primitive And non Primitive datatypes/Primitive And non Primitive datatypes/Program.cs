ace8a8bb6c0c687b1d35404c5a551167e4198297
Somanath Barge <somanath@nimap.com>
2023-06-08 18:21:15 +0530

    8th que

---
+++ Primitive And non Primitive datatypes/Primitive And non Primitive datatypes/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primitive_And_non_Primitive_datatypes
+{
+    internal class Program
+    {
+        //struct
+        public struct Add
+        {
+            public int a;
+            public int b;
+            public int c;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("following are the primitive data types in c# \nrange of byte is " + byte.MinValue + " to "+byte.MaxValue);
+            Console.WriteLine("range of int is from "+int.MinValue+" to "+int.MaxValue);
+            Console.WriteLine("range of double is from "+double.MinValue+" to "+double.MaxValue);
+            Console.WriteLine("range of long is from "+long.MinValue+" to "+long.MaxValue);
+            Console.WriteLine("range of decimal"+decimal.MinValue+" to "+decimal.MaxValue);
+            Console.WriteLine("range of short is from "+short.MinValue+" to "+short.MaxValue);
+            Console.WriteLine("range of float is from "+float.MinValue+" to "+float.MaxValue);
+            Console.WriteLine("range of char is from "+char.MinValue+" to "+char.MaxValue);
+            Console.WriteLine(true +" "+ false);
+
+            //followings are non primitive datatypes
+            string s = "Soma";
+            s.Trim('o','a');
+            Console.WriteLine(s);
+
+            //array datatype
+            int[] arr=new int[3];
+            arr[0] = 1;
+            arr[1] = 2;
+            arr[2] = 3;
+            Console.WriteLine("Elements in Array are ");
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            //stack datatype
+            Stack<int> stack = new Stack<int>();
+            stack.Push(6);
+            stack.Push(5);
+            stack.Push(4);
+            Console.WriteLine("stack is");
+            foreach(int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            //datatype queue
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            Console.WriteLine("Queue is");
+            foreach(int i in queue)
+            {
+                Console.WriteLine(i);
+            }
+            //LinkedList 
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(10);
+            list.AddLast(11);
+            list.AddLast(12);
+            Console.WriteLine("Linked list is");
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            //struct
+            Add add;
+            add.a = 10;
+            add.b = 11;
+            add.c = add.a + add.b;
+            Console.WriteLine("This is struct "+add.c);
+            
+            //struct allow you to encapsulate the related information into single unit and it is immutable
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Primitive And non Primitive datatypes/Primitive And non Primitive datatypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Primitive And non Primitive datatypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Primitive And non Primitive datatypes")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ec3cd2a-cf7c-47a2-ab9f-eaadea32a932")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
